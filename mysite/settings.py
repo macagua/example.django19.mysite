135ec93500b8d9f68c221d2e087602d3d1d27fae
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-02-02 15:18:13 -0400

    Added Django1.9 official tutorial with Polls App

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
-    'polls',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
--- polls/admin.py
+++ polls/admin.py
@@ -3,3 +3,23 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
