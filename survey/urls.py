9727394d0a613667de86df38ddee9dc796359f00
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-02-05 02:47:37 -0400

    View based in class, option 2 to use views in django rest framework

--- survey/urls.py
+++ survey/urls.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import url
+from django.conf.urls import patterns, include, url
+from rest_framework.urlpatterns import format_suffix_patterns
 from survey import views
 
 
 urlpatterns = [
-    url(r'^list/$', views.survey_list, name='survey_list'),
-    url(r'^detail/(?P<pk>[0-9]+)/$', views.survey_detail, name='survey_detail'),
+    url(r'^list/$', views.SurveyList.as_view(), name='survey_list'),
+    url(r'^detail/(?P<pk>[0-9]+)/$', views.SurveyDetail.as_view(), name='survey_detail'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- survey/views.py
+++ survey/views.py
@@ -1,54 +1,57 @@
 # -*- coding: utf-8 -*-
 
+from django.http import Http404
 from rest_framework import status
 from rest_framework import permissions
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from survey.models import Survey
 from survey.serializers import SurveySerializer
 
 
-@api_view(['GET', 'POST'])
-def survey_list(request):
+class SurveyList(APIView):
     '''
     List all surveys, or create a new survey
     '''
-    if request.method == 'GET':
+    def get(self, request, format=None):
         survey = Survey.objects.all()
         serializer = SurveySerializer(survey, many=True)
         return Response(serializer.data)
-    elif request.method == 'POST':
+
+    def post(self, request, format=None):
         serializer = SurveySerializer(data=request.data)
-    
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    else:
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-@api_view(['GET', 'PUT', 'DELETE'])
-@permission_classes((permissions.AllowAny,))
-def survey_detail(request, pk):
+class SurveyDetail(APIView):
     '''
     Get, update, or delete a specific survey
     '''
-    try:
-        survey = Survey.objects.get(pk=pk)
-    except Survey.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == 'GET':
+    def get_object(self, pk):
+        try:
+            return Survey.objects.get(pk=pk)
+        except Survey.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        survey = self.get_object(pk)
         serializer = SurveySerializer(survey)
         return Response(serializer.data)
-    
-    if request.method == 'PUT':
+
+    def put(self, request, pk, format=None):
+        survey = self.get_object(pk)
         serializer = SurveySerializer(survey, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
+
+    def delete(self, request, pk, format=None):
+        survey = self.get_object(pk)
         survey.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
